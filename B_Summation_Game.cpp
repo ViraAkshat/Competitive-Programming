0ce1f4ae0bb24c40d8cffac9485998017739d7ae
Akshat Vira <akshatvira812@gmail.com>
2024-02-12 23:49:24 +0530

    First upsolve

--- B_Summation_Game.cpp
+++ B_Summation_Game.cpp
@@ -8,7 +8,7 @@ int main()
     while (t--)
     {
         int n, k, x;
-        vector<int> arr;
+
         string line;
         int data;
         getline(cin >> ws, line);
@@ -16,21 +16,40 @@ int main()
         words >> n;
         words >> k;
         words >> x;
+        vector<int> arr(n + 1, 0);
 
-        getline(cin >> ws, line);
-        words.str(line);
-        int sum = 0;
-        while (words >> data)
+        string line2;
+        getline(cin >> ws, line2);
+        stringstream words2(line2);
+        int idx = 1;
+        while (words2 >> data)
         {
-            arr.push_back(data);
-            sum += data;
+            arr[idx] = data;
+            idx++;
         }
 
         sort(arr.begin(), arr.end());
 
-        while (k--)
+        int pref[n + 1];
+        pref[0] = 0;
+        for (int i = 1; i <= n; i++)
         {
+            pref[i] = arr[i] + pref[i - 1];
         }
+
+        int ans = INT_MIN;
+
+        for (int i = n; i >= 0; i--)
+        {
+            int ch = n - i;
+            if (ch > k)
+                break;
+            int r = min(i, x);
+            int sum = (pref[i - r] - (pref[i] - pref[i - r]));
+            ans = max(ans, sum);
+        }
+
+        cout << ans << endl;
     }
 
     return 0;
